2fa33347cefdaf77194b9c145b3663565bc95973
leohut <132941625+leohut@users.noreply.github.com>
2023-09-01 15:07:50 +0200

    other commit

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class AddressBook(UserDict):
             ):
                 results.append(record)
         return results  
+    
     def add_record(self, record):
         self.data[record.name.value] = record
         
